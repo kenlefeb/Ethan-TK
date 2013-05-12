f3c78b29c051ff05b61af03803243b6e96ad0993
Kenneth LeFebvre <kenneth@lefebvre.us>
2013-05-12 15:27:28 -0400

    Finished the first lesson and the quiz. Still need to do the homework.

---
+++ src/TeachingKids/01.SimpleSquare/SimpleSquare.cs
@@ -0,0 +1,40 @@
+﻿using SmallBasicFun;
+
+namespace SimpleSquare
+{
+	public class SimpleSquare
+	{
+		/// <summary>
+		/// This method starts the program.
+		/// </summary>
+		public static void Main()
+		{
+			Tortoise.Show();
+			Tortoise.SetSpeed(10);
+			for (int i = 0; i < 360; i++)
+			{
+				switch (i)
+				{
+					case 0:
+						Tortoise.SetPenColor("green");
+						Tortoise.SetSpeed(10);
+						break;
+					case 2:
+						Tortoise.SetPenColor("blue");
+						Tortoise.SetSpeed(10);
+						break;
+					case 1:
+						Tortoise.SetPenColor("pink");
+						Tortoise.SetSpeed(10);
+						break;
+					case 3:
+						Tortoise.SetPenColor("red");
+						break;
+				}
+				Tortoise.Move(5);
+				Tortoise.Turn(1);
+			}
+			
+		}
+	}
+}
---
+++ src/TeachingKids/01.SimpleSquare/SimpleSquareQuiz.cs
@@ -0,0 +1,40 @@
+﻿using SmallBasicFun;
+
+namespace SimpleSquare
+{
+	public class SimpleSquareQuiz
+	{
+		private static void question1()
+		{
+			// Turn the tortoise 1/5 of 360 degrees to the right
+			Tortoise.Turn(360/5);
+		}
+
+		private static void question2()
+		{
+			// Move the tortoise 110 pixels
+		Tortoise.Move(110);
+		}
+
+		private static void question3()
+		{
+			//  Change the color the tortoise draws to yellow
+		Tortoise.SetPenColor("yellow");
+		}
+
+		private static void question4()
+		{
+			//  Change the width of the line the tortoise draws to 5 pixels
+		Tortoise.SetPenWidth(5);
+		}
+
+		#region Ignore
+
+		public static void Main()
+		{
+			Quiz.StartSquareQuizAt += question1;
+		}
+
+		#endregion
+	}
+}
